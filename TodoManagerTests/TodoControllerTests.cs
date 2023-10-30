9dbc67a688f1111d3f443744a00d8d010b8924ce
Bence Tóth <b.toth@tricentis.com>
2023-10-30 21:51:19 +0100

    Unit tests

---
+++ TodoManagerTests/TodoControllerTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using TodoManager.Controllers;
+using TodoManager.DataAccess;
+using TodoManager.Models;
+
+namespace TodoManagerTests
+{
+    public class TodoControllerTest
+    {
+        [Fact]
+        public async Task WhenPostBodyIsCorrect_ShouldReturnCreatedTodo()
+        {
+            // Arrange
+            var mockTodoRepository = new Mock<ITodoRepository>();
+            var todoController = new TodoController(mockTodoRepository.Object);
+
+            var todoDto = new TodoDto
+            {
+                Description = "Test Todo",
+                User = "Test User",
+                IsDone = false
+            };
+
+            var createdTodo = new Todo
+            {
+                Id = "1",
+                Description = "Test Todo",
+                User = "Test User",
+                IsDone = false
+            };
+
+            mockTodoRepository.Setup(r => r.CreateTodoAsync(It.IsAny<TodoDto>())).ReturnsAsync(createdTodo);
+
+            // Act
+            var result = await todoController.CreateTodoElementAsync(todoDto);
+
+            // Assert
+            result.Should().BeOfType<OkObjectResult>();
+            var okResult = (OkObjectResult)result;
+
+            okResult.Value.Should().BeOfType<Todo>();
+            var model = (Todo)okResult.Value!;
+
+            model.Id.Should().Be("1");
+            model.Description.Should().Be("Test Todo");
+            model.User.Should().Be("Test User");
+            model.IsDone.Should().BeFalse();
+        }
+    }
+}
---
+++ TodoManagerTests/TodoRepositoryTests.cs
@@ -0,0 +1,55 @@
+﻿
+using FluentAssertions;
+using Microsoft.Azure.Cosmos;
+using Moq;
+using TodoManager.DataAccess;
+using TodoManager.Models;
+
+namespace TodoManagerTests
+{
+    public class TodoRepositoryTest
+    {
+        [Fact]
+        public async Task CreateTodoAsync_ShouldReturnCreatedTodo()
+        {
+            // Arrange
+            var mockCosmosClient = new Mock<CosmosClient>();
+            var mockDatabase = new Mock<Database>();
+            var mockContainer = new Mock<Container>();
+            var mockResponse = new Mock<ItemResponse<Todo>>();
+            mockCosmosClient.Setup(c => c.GetDatabase("databaseName")).Returns(mockDatabase.Object);
+            mockDatabase.Setup(d => d.GetContainer("containerName")).Returns(mockContainer.Object);
+
+            var todoDto = new TodoDto
+            {
+                User = "Test User",
+                Description = "Test Todo",
+                IsDone = true
+            };
+
+            var todo = new Todo
+            {
+                Id = Guid.NewGuid().ToString(),
+                User = "Test User",
+                Description = "Test Todo",
+                IsDone = true
+            };
+            mockResponse.Setup(r => r.Resource).Returns(todo);
+
+            mockContainer.Setup(c => c.CreateItemAsync(It.Is<Todo>(t => t.User == "Test User" && t.Description == "Test Todo" && t.IsDone), null, null, default))
+                .ReturnsAsync(mockResponse.Object);
+
+            var todoRepository = new TodoRepository(mockCosmosClient.Object, "databaseName", "containerName");
+
+            // Act
+            var createdTodo = await todoRepository.CreateTodoAsync(todoDto);
+
+            // Assert
+            createdTodo.Should().NotBeNull();
+            Guid.TryParse(createdTodo.Id, out _).Should().BeTrue();
+            createdTodo.User.Should().Be("Test User");
+            createdTodo.Description.Should().Be("Test Todo");
+            createdTodo.IsDone.Should().BeTrue();
+        }
+    }
+}
